00b3f3123d4e7ad98140d1c3e8e8ac0f74966610
何某本人 <709270121@qq.com>
2024-01-03 14:38:12 +0800

    项目初始化,maven使用版本3.6.1，然后Java使用1.8，spring boot使用2.1.8

---
+++ gulimall-product/src/test/java/com/atgulimall/newgulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atgulimall.newgulimall.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
